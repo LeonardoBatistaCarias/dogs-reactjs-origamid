a23628b7ddfddcc755705d9b4daa826b763989d5
LeonardoBatistaCarias <leonardo1carias@gmail.com>
2024-01-08 20:28:24 -0300

    final project components

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,30 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './components/Home';
 import Login from './components/Login/Login';
+import { UserStorage } from './UserContext';
+import User from './components/User/User';
+import ProtectedRoute from './components/Helper/ProtectedRoute';
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login/*" element={<Login />} />
-        </Routes>
-        <Footer />
+        <UserStorage>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="login/*" element={<Login />} />
+            <Route
+              path="conta/*"
+              element={
+                <ProtectedRoute>
+                  <User />
+                </ProtectedRoute>
+              }
+            />
+          </Routes>
+          <Footer />
+        </UserStorage>
       </BrowserRouter>
     </div>
   );
