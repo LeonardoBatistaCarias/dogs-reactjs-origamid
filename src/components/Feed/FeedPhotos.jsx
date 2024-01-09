84d92fcac61e8cf6269f7cf4d7310aebc4f43fb6
LeonardoBatistaCarias <leonardo1carias@gmail.com>
2024-01-09 16:33:01 -0300

    Photo, Comments, Loading y infinite loading components

--- src/components/Feed/FeedPhotos.jsx
+++ src/components/Feed/FeedPhotos.jsx
@@ -6,39 +6,38 @@ import { PHOTOS_GET } from '../../api';
 import Loading from '../Helper/Loading';
 import styles from './FeedPhotos.module.css';
 
-const FeedPhotos = ({ setModalPhoto }) => {
+const FeedPhotos = ({ page, user, setModalPhoto, setInfinite }) => {
   const { data, loading, error, request } = useFetch();
 
   React.useEffect(() => {
     async function fetchPhotos() {
-      const { url, options } = PHOTOS_GET({ page: 1, total: 6, user: 0 });
-      const { json } = await request(url, options);
-      console.log(json);
+      const total = 6;
+      const { url, options } = PHOTOS_GET({
+        page: page,
+        total: total,
+        user: user,
+      });
+      const { response, json } = await request(url, options);
+      if (response && response.ok && json.length < total) setInfinite(false);
     }
     fetchPhotos();
-  }, [request]);
+  }, [request, user, page, setInfinite]);
 
   if (error) return <Error error={error} />;
   if (loading) return <Loading />;
-  if (data)
+  if (data) {
     return (
       <ul className={`${styles.feed} animeLeft`}>
-        <FeedPhotosItem
-          key={data[0].id}
-          photo={data[0]}
-          setModalPhoto={setModalPhoto}
-        />
-        ;
-        {data.forEach((photo) => {
+        {data.map((photo) => (
           <FeedPhotosItem
             key={photo.id}
             photo={photo}
             setModalPhoto={setModalPhoto}
-          />;
-        })}
+          />
+        ))}
       </ul>
     );
-  else return null;
+  } else return null;
 };
 
 export default FeedPhotos;
